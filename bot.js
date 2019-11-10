d3c6948fdb8ae43b2cd8b5cdfed351e28a59da50
Artemis <Osmosis.orb@gmail.com>
2019-11-10 12:55:41 +0000

    Update
    Push WL urls to config,
    Add item names to accepted trades

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const SteamUser = require("steam-user");
 const SteamTotp = require("steam-totp");
+const webhook = require("webhook-discord")
 const SteamCommunity = require("steamcommunity");
 const TradeOfferManager = require("steam-tradeoffer-manager");
 const config = require("/home/bot/SteamBot/data/config.json");
@@ -10,6 +11,7 @@ const manager = new TradeOfferManager({
     community: community,
     language: "en"
 });
+var userContacted = [];
  
 const logOnOptions = {
     accountName: config.username,
@@ -22,7 +24,7 @@ client.logOn(logOnOptions);
 client.on("loggedOn", () => {
     client.setPersona(1);
     client.getNicknames(() => {
-        console.log(`[${new Date(new Date().getTime()).toLocaleString()}] Logged in as ${client.accountInfo.name} / ${client.steamID}`);
+        log_msg(`Logged in as ${client.accountInfo.name} / ${client.steamID}`);
         checkFriendRequests();
     });
     
@@ -36,8 +38,10 @@ client.on("webSession", (sessionid, cookies) => {
 
 manager.on("newOffer", (offer) => { //accept one sided, incoming trade offers
     if (offer.itemsToGive.length == 0 && offer.itemsToReceive.length > 0) {
-        console.log(`[${new Date(new Date().getTime()).toLocaleString()}] Accepted Offer #${offer.id} from ${offer.partner.getSteamID64()}.`);
+        log_msg(`Accepted Offer #${offer.id} from ${offer.partner.getSteamID64()}.`);
         offer.accept();
+    } else {
+        log_msg(`Incoming Offer #${offer.id} from ${offer.partner.getSteamID64()}.`);
     }
 });
 
@@ -45,12 +49,17 @@ manager.on("receivedOfferChanged", function(offer, oldState) { //log accepted tr
     if (offer.state == TradeOfferManager.ETradeOfferState.Accepted) {
         offer.getExchangeDetails((err, status, tradeInitTime, receivedItems, sentItems) => {
             if (err) {
-                console.log(`[${new Date(new Date().getTime()).toLocaleString()}] Error ${err}`);
+                log_msg(`Error ${err}`);
                 return;
             }
             let newReceivedItems = receivedItems.map(item => item.new_assetid);
             let newSentItems = sentItems.map(item => item.new_assetid);
-            console.log(`[${new Date(new Date().getTime()).toLocaleString()}] Status: ${TradeOfferManager.ETradeStatus[status]}\nReceived ${newReceivedItems.length} items\nSent ${newSentItems.length} items.`);
+            let newReceivedItemNames = "```";
+            for (var i = 0; i < receivedItems.length; i++) {
+                newReceivedItemNames+= receivedItems[i].market_name+"\n";
+            }
+            newReceivedItemNames+="```"
+            log_msg(`Status: ${TradeOfferManager.ETradeStatus[status]}\n+${newReceivedItems.length}/-${newSentItems.length}\nItems Recieved: ${newReceivedItemNames}`);
         });
     }
 });
@@ -61,25 +70,44 @@ client.on("friendRelationship", function(steamID, relationship) {
     }
 });
 
+client.on('friendMessage', function(steamID, message) {
+    if(message.includes("[/tradeoffer]") || message.includes("Invited you to play a game!")){
+        return
+    }
+    client.getPersonas([steamID], function(err, personas) {
+        let persona = personas[steamID.getSteamID64()];
+        let name = persona ? persona.player_name : ("[" + steamID.getSteamID64() + "]");
+        let currentTime = new Date().getTime()/1000;
+        let nextMessageTime = userContacted[steamID]+3600;
+        if (userContacted[steamID] == undefined || currentTime >= nextMessageTime){ //prevent people messaging me multiple times from being spammed with the automated message
+            userContacted[steamID] = currentTime;
+            client.chatMessage(steamID, `Hi ${name}, I don't tend to check my steam messages. You're better contacting me via discord (Artemis#1237). This is an automated message.`);
+            forwardSteamDM(`${name} (${steamID}) : ${message}\n(Auto-replied)`);
+        } else {
+            forwardSteamDM(`${name} (${steamID}) : ${message}`);
+        }
+    });
+});
+
 function checkRequest(steamID) {
     client.getSteamLevels([steamID], function(err, results){
         if (err) {
-            console.log(`[${new Date(new Date().getTime()).toLocaleString()}] Error ${err}`);
+            log_msg(`Error ${err}`);
             return;
         } else {
             let userLevel = results[steamID];
             if (userLevel < 5) {
                 client.removeFriend(steamID);
-                console.log(`[${new Date(new Date().getTime()).toLocaleString()}] Ignored friend request from ${steamID} due to their level (${userLevel})`);
+                log_msg(`Ignored friend request from ${steamID} due to their level (${userLevel})`);
             } else {
-                console.log(`[${new Date(new Date().getTime()).toLocaleString()}] Incoming friend request from ${steamID}.`);
+                log_msg(`Incoming friend request from ${steamID}.`);
             }
         }
     });
 }
 
 function checkFriendRequests(){
-    console.log(`[${new Date(new Date().getTime()).toLocaleString()}] Checking friend requests.`);
+    log_msg(`Checking friend requests.`);
     for(let user in client.myFriends){
         if(client.myFriends[user] == 2){
             checkRequest(user);
@@ -87,3 +115,34 @@ function checkFriendRequests(){
     }
 }
 
+function log_msg(msg){
+    try{
+        const Hook = new webhook.Webhook(config.wh_logs);
+        var wh_msg = new webhook.MessageBuilder()
+            .setName("BotLog")
+            .setColor("#00ff00")
+            .setDescription(msg)
+            .setFooter(new Date(new Date().getTime()).toLocaleString());
+        Hook.send(wh_msg);
+    } catch(e) {
+        log_error(e)
+    }
+}
+function forwardSteamDM(msg){
+    try{
+        const Hook = new webhook.Webhook(config.wh_say);
+        var wh_msg = new webhook.MessageBuilder()
+            .setName("SteamMsg")
+            .setColor("#00ff00")
+            .setDescription(msg)
+            .setFooter(new Date(new Date().getTime()).toLocaleString());
+        Hook.send(wh_msg);
+    } catch(e) {
+        log_error(e)
+    }
+}
+
+function log_error(msg){
+    const Hook = new webhook.Webhook(config.wh_logs);
+    Hook.err("ErrorLog", msg);
+}
